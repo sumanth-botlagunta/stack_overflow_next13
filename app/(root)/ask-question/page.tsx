b1883d2f01fb6701776da3275724a8b74a21cee0
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-10-14 11:43:06 +0530

    webhook deploy

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,12 +1,11 @@
 import Question from '@/components/forms/Question';
 import { getUserById } from '@/lib/actions/user.action';
+import { auth } from '@clerk/nextjs';
 import { Schema } from 'mongoose';
 import { redirect } from 'next/navigation';
 
 const Page = async () => {
-  // const { userId } = auth();
-
-  const userId = 'unique123';
+  const { userId } = auth();
   if (!userId) redirect('/sign-in');
 
   const mongoUserId: Schema.Types.ObjectId = await getUserById(userId);

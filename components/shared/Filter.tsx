3b9edf8900221a33065b326af703745e7915588c
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-10-17 11:16:14 +0530

    tags page

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -30,7 +30,7 @@ const Filter = ({ filters, otherClasses, containerClasses }: Props) => {
           </div>
         </SelectTrigger>
         <SelectContent>
-          <SelectGroup>
+          <SelectGroup className="background-light800_dark300 text-dark500_light700">
             {filters.map((item) => (
               <SelectItem key={item.value} value={item.value}>
                 {item.name}
